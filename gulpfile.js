24481bdfe6bcb81b2deee94b531111af2dd11d7b
Zachary Morel <morelz133@gmail.com>
2017-09-19 13:41:39 -0400

    wokring on Glob task running

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,13 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+
+gulp.task('hello', function() {
+    console.log('Hello Zell')
+});
+
+gulp.task('sass', function(){
+    return gulp.src('app/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+})
